b83f7f6880399192cbacdba54f300fdc9791568e
Mjturn <Mjturn1@proton.me>
2025-09-24 18:52:31 -0400

    revise script

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+cd
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+sudo pacman -Syu --noconfirm waybar hyprpaper
+
 git clone git@github.com:Mjturn/dotfiles.git ~/.dotfiles
+
+rm -r ~/.config/hypr
+rm -r ~/.config/kitty
+
 ln -s ~/.dotfiles/hypr ~/.config/hypr
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/nvim ~/.config/nvim
@@ -8,24 +19,17 @@ ln -s ~/.dotfiles/waybar ~/.config/waybar
 ln -s ~/.dotfiles/wofi ~/.config/wofi
 mv ~/.dotfiles/Wallpapers ~
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
 sudo pacman -Syu --noconfirm fish
-chsh -s /bin/fish
+chsh -s /usr/bin/fish
 
 sudo pacman -Syu --noconfirm dash
 sudo ln -sfT dash /usr/bin/sh
 yay -Syu --noconfirm dashbinsh
 
-sudo pacman -Syu --noconfirm neovim
 sudo pacman -Syu --noconfirm steam
 yay -Syu --noconfirm minecraft-launcher
 sudo pacman -Syu --noconfirm man
-sudo pacman -Syu --noconfirm unzip
 sudo pacman -Syu --noconfirm wl-clipboard
-sudo pacman -Syu --noconfirm ttf-fira-code ttf-jetbrains-mono
+sudo pacman -Syu --noconfirm ttf-fira-code ttf-jetbrains-mono otf-font-awesome
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+reboot
